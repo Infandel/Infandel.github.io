ff92d05c89edd72b9d850c042308a6503bdff6fe
Infandel <tomahawk92@gmail.com>
2021-07-12 15:35:23 +0500

    Some useless strings removal

--- app/routes/bicycle.js
+++ app/routes/bicycle.js
@@ -1,7 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { Promise } from 'rsvp';
-import { later } from '@ember/runloop';
 
 export default Route.extend({
   dataService: service('data'),
