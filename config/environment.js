630025779da1483751f1e79f7b68fa435e451e70
Infandel <tomahawk92@gmail.com>
2021-07-14 21:19:05 +0500

    Trying to do another prod build

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/bike-rent-app/';
     // here you can enable a production-specific feature
   }
 
